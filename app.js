a9149d197e692724b4ec407d1e92f8e2231f86a0
Nate Nault <nate@nathannault.com>
2017-03-14 06:56:49 +0800

    Setup basic server

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var express = require('express');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+  console.log('Express server now listening on port: ' + port);
+});
