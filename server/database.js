39204a50b61d553eacad1ee48a14dc2e7e0dd1be
Nate Nault <nate@nathannault.com>
2017-03-15 07:39:11 +0800

    Add mongodb config

--- server/database.js
+++ server/database.js
@@ -2,6 +2,6 @@
 
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/snippet-saver');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/snippet-saver');
 
 // TODO: Add success/error message
