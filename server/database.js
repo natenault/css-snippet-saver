e246d2756bc7801b87f4d3ceabf78f357ae98e6d
Nate Nault <nate@nathannault.com>
2017-03-15 08:05:02 +0800

    Add connection uri

--- server/database.js
+++ server/database.js
@@ -2,6 +2,11 @@
 
 var mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/snippet-saver');
+var uri;
+
+process.env.PORT ? uri = 'mongodb://heroku_j1kdj66m:im2b5qoui4v5ocoprkit65v8l3@ds163377.mlab.com:63377/heroku_j1kdj66m' : uri = 'mongodb://localhost/snippet-saver';
+
+mongoose.connect(uri);
 
 // TODO: Add success/error message
+// mongodb://heroku_j1kdj66m:im2b5qoui4v5ocoprkit65v8l3@ds163377.mlab.com:63377/heroku_j1kdj66m
